279096a928a4cb4c3af846b4ac11d5f2556798cc
Amaury <Amaury.doux@gmail.com>
2018-08-19 14:53:50 +0200

    Api call successful

---
+++ public/components/home/apiCall.js
@@ -0,0 +1,19 @@
+var CLIENT_ID = 'X0HZ3VKUHS1YJH1CLP1RIUNWICGLFR0KBPSCHOMCQ2F3CQCW';
+var CLIENT_SECRET = 'V5KJKQZQCD120KRS4ALIGXVQKEMUF12BTNB1CBJWCPZOJIGV';
+
+function callFoursquareNew(){
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+  var request = new XMLHttpRequest();
+
+  // Open a new connection, using the GET request on the URL endpoint
+  request.open('GET','https://api.foursquare.com/v2/venues/explore?client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee', true);
+
+  request.onload = function () {
+    var data = JSON.parse(this.response);
+    console.log(data)
+    // Begin accessing JSON data here
+    }
+
+  // Send request
+  request.send();
+}
---
+++ public/components/home/home.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('dolisttest')
+
+.component('home', {
+    templateUrl: '/components/home/home.html',
+    controller: Home
+})
+
+function Home() {
+    // Controller
+    
+}
